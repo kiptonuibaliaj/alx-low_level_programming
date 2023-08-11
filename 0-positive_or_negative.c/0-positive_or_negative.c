4b507e584f5b3ed553eccbc7fd1197bed1ffe2d4
Baliaj <kiptonuibaliaj@gmail.com>
2023-08-11 16:26:32 -0700

    C - Variables, if, else, while

---
+++ 0-positive_or_negative.c/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+/*
+ * File: 0-positive_or_negative.c
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints and tests a radom number if positive or neg
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+
+	return (0);
+}
---
+++ 0-positive_or_negative.c/6-print_numberz.c
@@ -0,0 +1,22 @@
+/*
+ * File: 6-print_numberz.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - TL DR
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
